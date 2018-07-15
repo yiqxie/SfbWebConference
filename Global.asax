422fc9fe38f8af680b8205126fdfa069f35d8de3
yiqxie <xyq0324@126.com>
2018-07-15 20:55:48 +0800

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SFBWebService.WebApiApplication" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+using SFBConference.Globals;
+
+namespace SFBWebService
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            SFBConferenceGlobals.Initialize();
+
+        }
+    }
+}
